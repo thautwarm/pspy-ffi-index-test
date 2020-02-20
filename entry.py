dc00e4ab6ab33efa6bcad1a8fec45e89355accc7
thautawarm <twshere@outlook.com>
2020-02-21 02:24:54 +0900

    up

--- entry.py
+++ entry.py
@@ -3,7 +3,8 @@ from purescripto.ffi_utilities import auto_link_repo
 
 
 def solve(package_name: str, versions: List[int]):
-    if package_name == 'purescript-prelude':
-        return auto_link_repo(
+    if package_name == 'prelude':
+        repo = auto_link_repo(
             r"https://github.com/purescript-python/purescript-prelude.py")
+        return repo.working_dir
     raise NotImplementedError
