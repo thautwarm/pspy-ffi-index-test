51af8da918cff6d60a839bed51887636485f172b
thautawarm <twshere@outlook.com>
2020-02-21 19:28:24 +0900

    mirror spec changed

---
+++ README.md
@@ -0,0 +1,20 @@
+This is a PureScript FFI provider mirror for the Python backend.
+
+Making such a mirror is simple, by implementing following interfaces:
+1. Providing an `entry.py` in the Git repo's root.
+2. In `entry.py`, provide a function `solve(package_name: str, versions: List[int]) -> str`,
+
+which takes
+
+- the name of package.  
+  e.g., for `purescript-prelude`, you'll receive `prelude`.
+
+- the version parts
+  
+  e.g., for `v1.1.1`, you'll get `[1, 1, 1]`.
+
+and return
+
+- a URL of a Git repo providing the Python FFI files.
+
+  e.g., for [purescript-console](https://github.com/purescript/purescript-console), we can make this [purescript-console.py](`https://github.com/purescript-python/purescript-console.py`) for former's Python FFI files).
--- entry.py
+++ entry.py
@@ -1,16 +1,14 @@
 from typing import List
-from purescripto.ffi_utilities import auto_link_repo
 
 
-_repo_link_map = {
+_repo_url_link_map = {
     'prelude': r"https://github.com/purescript-python/purescript-prelude.py",
     'effect': r"https://github.com/purescript-python/purescript-effect.py",
     'console': r"https://github.com/purescript-python/purescript-console.py"
 }
 
-def solve(package_name: str, versions: List[int]):
-    repo_link = _repo_link_map.get(package_name)
-    if repo_link:
-        repo = auto_link_repo(repo_link)
-        return repo.working_dir
+def solve(package_name: str, versions: List[int]) -> str:
+    repo_url = _repo_url_link_map.get(package_name)
+    if repo_url:
+        return repo_url
     raise NotImplementedError("package {} is not supported by this mirror".format(package_name))
