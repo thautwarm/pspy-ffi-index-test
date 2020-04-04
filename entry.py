51d86164c3232e1a9a73d56dec68e2f0ea1dc506
Xiang <hx.hongxiang@gmail.com>
2020-04-04 15:10:16 +0800

    feature(): add assert, st, unsafe-coerce, unfoldable, format with black

--- entry.py
+++ entry.py
@@ -1,17 +1,21 @@
 from typing import List
 
 _repo_url_link_map = {
-    'prelude': r"https://github.com/purescript-python/purescript-prelude.py",
-    'effect': r"https://github.com/purescript-python/purescript-effect.py",
-    'console': r"https://github.com/purescript-python/purescript-console.py",
-    'control': r"https://github.com/purescript-python/purescript-control.py",
-    'enums': r"https://github.com/purescript-python/purescript-enums.py",
+    "prelude": r"https://github.com/purescript-python/purescript-prelude.py",
+    "effect": r"https://github.com/purescript-python/purescript-effect.py",
+    "console": r"https://github.com/purescript-python/purescript-console.py",
+    "control": r"https://github.com/purescript-python/purescript-control.py",
+    "enums": r"https://github.com/purescript-python/purescript-enums.py",
     "foldable-traversable": r"https://github.com/purescript-python/purescript-foldable-traversable.py",
     "partial": r"https://github.com/purescript-python/purescript-partial.py",
     "show-python": r"https://github.com/purescript-python/purescript-show-python",
     "functions": r"https://github.com/purescript-python/purescript-functions.py",
     "nullable": r"https://github.com/purescript-python/purescript-nullable.py",
-    "refs": r"https://github.com/purescript-python/purescript-refs.py"
+    "refs": r"https://github.com/purescript-python/purescript-refs.py",
+    "assert": r"https://github.com/purescript-python/purescript-assert.py",
+    "st": r"https://github.com/purescript-python/purescript-st.py",
+    "unsafe-coerce": r"https://github.com/purescript-python/purescript-unsafe-coerce.py",
+    "unfoldable": r"https://github.com/purescript-python/purescript-unfoldable.py",
 }
 
 
@@ -20,4 +24,5 @@ def solve(package_name: str, versions: List[int]) -> str:
     if repo_url:
         return repo_url
     raise NotImplementedError(
-        "package {} is not supported by this mirror".format(package_name))
+        "package {} is not supported by this mirror".format(package_name)
+    )
